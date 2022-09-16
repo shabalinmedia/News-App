43c3544c5262555a87b9ff90b4bd5156ee5ea3f8
Alex Shabalin <shabalin.media@gmail.com>
2022-09-16 16:02:15 +0600

    Add NewsViewMoled

---
+++ News App/ViewModel/NewsViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  NewsViewModel.swift
+//  News App
+//
+//  Created by Alexander Shabalin on 16/9/22.
+//
+
+import Foundation
+import Combine
+
+protocol NewsViewModel {
+    func getArticles()
+}
+
+class NewsViewModelImpl: ObservableObject, NewsViewModel {
+    
+    private let service: NewsService
+    
+    private(set) var articles = [Article]()
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published private(set) var state: ResultState = .loading
+    
+    init(service: NewsService) {
+        self.service = service
+    }
+    
+    func getArticles() {
+        
+        self.state = .loading
+        
+        let cancellable = service
+            .request(from: .getNews)
+            .sink { res in
+                switch res {
+                    
+                case .finished:
+                    self.state = .success(content: self.articles)
+                break
+                case .failure(let error):
+                    self.state = .failed(error: error)
+                break
+                }
+            } receiveValue: { response in
+                self.articles = response.articles
+            }
+        
+        self.cancellables.insert(cancellable)
+
+    }
+}
