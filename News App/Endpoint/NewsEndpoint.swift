e620729a1b17827a636c36a75b8961ccb59355e4
Alex Shabalin <shabalin.media@gmail.com>
2022-09-16 14:21:31 +0600

    Add Endpoint

--- News App/Endpoint/NewsEndpoint.swift
+++ News App/Endpoint/NewsEndpoint.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+protocol APIBuilder {
+    var urlRequest: URLRequest { get }
+    var baseUrl: URL { get }
+    var path: String { get }
+}
+
+enum NewsAPI {
+    case getNews
+}
+
+extension NewsAPI: APIBuilder {
+    
+    var baseUrl: URL {
+        switch self {
+        case .getNews:
+            return URL(string: "https://api.lil.software/")!
+        }
+    }
+    
+    var path: String {
+        return "/news"
+    }
+    
+    var urlRequest: URLRequest {
+        return URLRequest(url: self.baseUrl.appendingPathComponent(self.path))
+    }
+}
