9cafaeaa798531accd3eb91c7a97aae3803e8858
Alex Shabalin <shabalin.media@gmail.com>
2022-09-18 23:38:23 +0600

    Add ContentView

--- News App/News_AppApp.swift
+++ News App/News_AppApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct News_AppApp: App {
+struct NewsAPIApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            ContentView()
         }
     }
 }
---
+++ News App/Views/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  HomeView.swift
+//  News App
+//
+//  Created by Alexander Shabalin on 16/9/22.
+//
+
+import SwiftUI
+import Combine
+
+struct ContentView: View {
+    
+    @Environment(\.openURL) var openURL
+    @StateObject var viewModel: NewsViewModelImpl = NewsViewModelImpl(service: NewsServiceImpl())
+    
+    var body: some View {
+        
+        Group {
+            
+            switch viewModel.state {
+            case .loading:
+                ProgressView()
+            case .failed(let error):
+                ErrorView(error: error) {
+                    self.viewModel.getArticles()
+                }
+            case .success(let content):
+                NavigationView {
+                    List(content) { article in
+                        ArticleView(article: article)
+                            .onTapGesture {
+                                load(url: article.url)
+                            }
+                    }
+                    .navigationBarTitle("News")
+                }
+            }
+        }
+        .onAppear {
+            self.viewModel.getArticles()
+        }
+    }
+    
+    func load(url: String?) {
+        guard let url = url,
+            let linkUrl = URL(string: url) else {
+            return
+        }
+        openURL(linkUrl)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
