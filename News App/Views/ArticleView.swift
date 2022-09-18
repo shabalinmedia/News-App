0810f01a4f65310c9c40e944f6a9a89b5975d94d
Alex Shabalin <shabalin.media@gmail.com>
2022-09-18 22:45:55 +0600

    Import URLImage

---
+++ News App/Views/ArticleView.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleView.swift
+//  News App
+//
+//  Created by Alexander Shabalin on 18/9/22.
+//
+
+import SwiftUI
+import URLImage
+
+struct ArticleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleView()
+    }
+}
